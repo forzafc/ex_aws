5d59cb2dfb7102dd9c214af41d806736c044fbf2
ybod <yuriibodarev@gmail.com>
2019-08-25 00:31:57 +0300

    Update deps

--- mix.exs
+++ mix.exs
@@ -4,28 +4,32 @@ defmodule ExAws.Mixfile do
   @version "1.1.0"
 
   def project do
-    [app: :ex_aws,
-     version: @version,
-     elixir: "~> 1.4",
-     elixirc_paths: elixirc_paths(Mix.env),
-     description: "AWS client. Currently supports Dynamo, DynamoStreams, EC2, Firehose, Kinesis, KMS, Lambda, RRDS, Route53, S3, SES, SNS, SQS, STS",
-     name: "ExAws",
-     source_url: "https://github.com/cargosense/ex_aws",
-     package: package(),
-     dialyzer: [flags: "--fullpath"],
-     deps: deps(),
-     docs: [main: "ExAws", source_ref: "v#{@version}",
-       source_url: "https://github.com/cargosense/ex_aws"]
-     ]
+    [
+      app: :ex_aws,
+      version: @version,
+      elixir: "~> 1.4",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      description:
+        "AWS client. Currently supports Dynamo, DynamoStreams, EC2, Firehose, Kinesis, KMS, Lambda, RRDS, Route53, S3, SES, SNS, SQS, STS",
+      name: "ExAws",
+      source_url: "https://github.com/cargosense/ex_aws",
+      package: package(),
+      dialyzer: [flags: "--fullpath"],
+      deps: deps(),
+      docs: [
+        main: "ExAws",
+        source_ref: "v#{@version}",
+        source_url: "https://github.com/cargosense/ex_aws"
+      ]
+    ]
   end
 
   def application do
-    [applications: [:logger, :crypto],
-     mod: {ExAws, []}]
+    [applications: [:logger, :crypto], mod: {ExAws, []}]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib",]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     deps(:test_dev)
@@ -33,23 +37,26 @@ defmodule ExAws.Mixfile do
 
   defp deps(:test_dev) do
     [
-      {:xml_builder, "~> 0.0.6", optional: true},
+      {:xml_builder, "~> 2.1", optional: true},
       {:sweet_xml, "~> 0.6", optional: true},
       {:ex_doc, "~> 0.14", only: :dev},
-      {:hackney, "~> 1.6.5", optional: true},
+      {:hackney, "~> 1.15.1", optional: true},
       {:poison, ">= 1.2.0", optional: true},
       {:jsx, "~> 2.8", optional: true},
-      {:dialyze, "~> 0.2.0", only: :dev},
-      {:bypass, "~> 0.1", only: :test},
-      {:configparser_ex, "~> 0.2.1", optional: true},
+      {:dialyze, "~> 0.2.1", only: :dev},
+      {:bypass, "~> 1.0.0", only: :test},
+      {:configparser_ex, "~> 4.0.0", optional: true}
     ]
   end
 
   defp package do
-    [description: "AWS client. Currently supports Dynamo, DynamoStreams, EC2, Firehose, Kinesis, KMS, Lambda, RRDS, Route53, S3, SES, SNS, SQS, STS",
-     files: ["lib", "config", "mix.exs", "README*"],
-     maintainers: ["Ben Wilson"],
-     licenses: ["MIT"],
-     links: %{github: "https://github.com/CargoSense/ex_aws"}]
+    [
+      description:
+        "AWS client. Currently supports Dynamo, DynamoStreams, EC2, Firehose, Kinesis, KMS, Lambda, RRDS, Route53, S3, SES, SNS, SQS, STS",
+      files: ["lib", "config", "mix.exs", "README*"],
+      maintainers: ["Ben Wilson"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/CargoSense/ex_aws"}
+    ]
   end
 end
